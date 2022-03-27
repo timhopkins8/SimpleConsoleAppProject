04bda5cf246ce918d053896ce8df2dac0f9f2e39
tim hopkins <tim.hopkins8@gmail.com>
2022-03-27 15:38:51 -0400

    create new branch to create calculate age option

---
+++ SimpleConsoleAppProject/CalculateAge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleAppProject
+{
+    public class CalculateAge 
+    {
+
+        private string _dob;
+        public CalculateAge(string dob)
+        {
+            _dob = dob;
+        }
+
+        public int CalculateAgeFromDob()
+        {
+            return 6;
+        }
+    }
+}
--- SimpleConsoleAppProject/Program.cs
+++ SimpleConsoleAppProject/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using SimpleConsoleAppProject;
+string option = "";
+do
+{
+   
+    Console.WriteLine("Select one of the options below or press 'q' to exit:");
+    Console.WriteLine();
+
+    Console.WriteLine("1: Calculate your age");
+    Console.WriteLine("2: Get Current Date and Time");
+    Console.WriteLine("q: Quit the program");
+    Console.Write("Enter the number from the menu: ");
+    option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            Console.WriteLine("Enter your DOB in this format MM/DD/YYYY");            
+            CalculateAge age = new CalculateAge(Console.ReadLine());
+            Console.WriteLine($"You are {age.CalculateAgeFromDob()} years old.");
+            break;
+        case "2":
+            Console.WriteLine($"The current date and time is {DateTime.Now.ToString("f")}");
+            break;
+        case "3":
+            Console.WriteLine("Program is ending....");
+            break;
+        default:
+            Console.WriteLine("Invalid options, Please enter a valid option.");
+            break;
+    }
+    Console.WriteLine();
+}
+while (option != "q");
